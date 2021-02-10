d33fdb090605529229908c5bac83335843dffb92
Marcel Koch <kontakt@marcelkoch.net>
2021-02-10 01:08:06 +0100

    play arround with tokio and j4rs

--- 09_language-bindings/rust/src/lib.rs
+++ 09_language-bindings/rust/src/lib.rs
@@ -2,8 +2,14 @@ use j4rs::prelude::*;
 use j4rs_derive::*;
 
 #[call_from_java("de.digitalfrontiers.rustbuch.dash.RustDasher.printHello")]
-fn print_hello() {
-    println!("Hello from the Rust world!");
+#[tokio::main]
+async fn print_hello() {
+    let handle = tokio::spawn(async {
+        "Hello from the Rust world!"
+    });
+
+    let out = handle.await.unwrap();
+    println!("GOT {}", out);
 }
 
 #[call_from_java("de.digitalfrontiers.rustbuch.dash.RustDasher.printHelloWithName")]
