c62c06fcba63833eee6022766f4bd2ab3aa95efa
Marcel Koch <kontakt@marcelkoch.net>
2021-04-20 19:37:05 +0200

    rename schoene_behauptungen

--- 05_testing/src/main.rs
+++ 05_testing/src/main.rs
@@ -5,7 +5,7 @@ mod kapitel_5_5_2_4_generische_methoden;
 mod kapitel_5_5_2_3_generische_traits;
 mod kapitel_5_5_2_6_module;
 mod kapitel_5_5_2_7_gemockte_module_verwenden;
-mod kapitel_5_6_1_schoene_behauptungen;
+mod kapitel_5_3_2_schoene_behauptungen;
 
 fn main() {
     println!("{}", kapitel_5_5_2_6_module::a_module::return_string());
