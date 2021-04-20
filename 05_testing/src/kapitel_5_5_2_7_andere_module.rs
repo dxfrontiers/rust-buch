878d99f066bfa31989b811847832eda78582617b
Marcel Koch <kontakt@marcelkoch.net>
2021-04-20 14:56:56 +0200

    make file names fit chapter numbers

--- 05_testing/src/kapitel_5_5_2_7_andere_module.rs
+++ 05_testing/src/kapitel_5_5_2_7_andere_module.rs
@@ -1,5 +1,5 @@
-use crate::kapitel_1_6_2_4_modules::mock_module_to_be_mocked;
-use crate::kapitel_1_6_2_4_modules::module_to_be_mocked;
+use crate::kapitel_5_5_2_6_module::mock_module_to_be_mocked;
+use crate::kapitel_5_5_2_6_module::module_to_be_mocked;
 
 #[cfg(test)]
 mod tests {
